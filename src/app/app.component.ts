5dd06cdcca1c996163f0df22bd878ac5be137d7b
Vi Nguyen <vi.nguyen@supremetech.vn>
2020-04-04 13:13:07 +0700

    add - modify styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,33 +10,34 @@ import { NgAuthenticationService } from 'ng-authentication';
 export class AppComponent implements OnInit, AfterViewInit {
 
   members: Array<object>;
+  loggedInAs: string;
 
   constructor(
     @Inject(NgAuthenticationService) private auth: NgAuthenticationService
   ) {
     this.members = [
       {
-        avatar: 'https://miro.medium.com/fit/c/80/80/2*wVB02Yf-s4OTFmqfpOO9rg.jpeg',
-        name: 'Vix Nguyen',
-        email: 'vinguyen.fitsgu@gmail.com',
-        posts: 5,
-        voted: false
-      },
-      {
         avatar: 'https://scontent-sin6-1.xx.fbcdn.net/v/t31.0-1/cp0/p50x50/11888514_936655183067191_3140575884179561539_o.jpg?_nc_cat=100&_nc_sid=dbb9e7&_nc_ohc=2mYieZY_5FsAX_EUtZ2&_nc_ht=scontent-sin6-1.xx&oh=97c7efd9b7f845b1e44608669cf3b66b&oe=5EA66978',
         name: 'Philippe Pham',
         email: 'phillipe.pham@gmail.com',
         posts: 7,
-        voted: false
-      },
-      {
+        voted: {}
+      }, {
+        avatar: 'https://miro.medium.com/fit/c/80/80/2*wVB02Yf-s4OTFmqfpOO9rg.jpeg',
+        name: 'Vix Nguyen',
+        email: 'vinguyen.fitsgu@gmail.com',
+        posts: 5,
+        voted: {}
+      }, {
         avatar: 'https://ca.slack-edge.com/TP6KHUJE7-UTHET5V3Q-814e8e42c291-192',
         name: 'Ricky Nguyen',
         email: 'ricky.nguyen@gmail.com',
         posts: 12,
-        voted: false
+        voted: {}
       }
     ];
+    const userInfo = this.auth.getUserInfo();
+    this.loggedInAs = userInfo ? userInfo.uid : '';
   }
 
   ngOnInit() {}
@@ -44,15 +45,18 @@ export class AppComponent implements OnInit, AfterViewInit {
   ngAfterViewInit() {}
 
   onLogin(email: string) {
-    this.auth.signinPasswordless({email});
+    this.auth.signinPasswordless({email}).then(() => {
+      this.loggedInAs = email;
+    });
   }
   
   onLogout() {
     this.auth.signout();
+    this.loggedInAs = '';
   }
 
   onVote(item: any) {
-    item.voted = true;
+    item.voted[this.loggedInAs] = true;
   }
 
 }
